e7b968c595ac4766f99dea546e9aad7b4abe0174
shevelev roman <shevelevroman12@gmail.com>
2023-12-09 02:41:20 +1000

    Initial commit

---
+++ project_ovp/boost.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class boost here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class boost extends Actor
+{
+    /**
+     * Act - do whatever the boost wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
